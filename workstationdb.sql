091704923a5908e9e3a5563b7205280ac5a6050c
Zokky2e <icro4.gaming@hotmail.com>
2025-01-08 16:23:47 +0100

    database changes

--- workstationdb.sql
+++ workstationdb.sql
@@ -3,13 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2025 at 11:12 PM
+-- Generation Time: Jan 08, 2025 at 04:21 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,16 +27,11 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
   `name` varchar(40) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `roles`
---
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `roles` (`id`, `name`, `description`) VALUES
 (1, 'admin', 'Admin'),
@@ -50,45 +44,21 @@ INSERT INTO `roles` (`id`, `name`, `description`) VALUES
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(40) NOT NULL,
   `firstname` varchar(40) NOT NULL,
   `lastname` varchar(40) NOT NULL,
   `password` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usernameIndex` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `roleId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `password`) VALUES
-(1, 'admin', 'admin', 'admin', '$2a$11$GFuY06mGGWP.Ao0ApvTsw.tfh/onw1K2leOgGgUkFTEaTrWkd4WXe');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_roles`
---
-
-DROP TABLE IF EXISTS `user_roles`;
-CREATE TABLE IF NOT EXISTS `user_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `roleId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_role_userId_FK` (`userId`) USING BTREE,
-  KEY `user_role_roleId_FK` (`roleId`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_roles`
---
-
-INSERT INTO `user_roles` (`id`, `userId`, `roleId`) VALUES
-(1, 1, 1);
+REPLACE INTO `users` (`id`, `username`, `firstname`, `lastname`, `password`, `roleId`) VALUES
+(1, 'admin', 'admin', 'admin', '$2a$11$GFuY06mGGWP.Ao0ApvTsw.tfh/onw1K2leOgGgUkFTEaTrWkd4WXe', 1);
 
 -- --------------------------------------------------------
 
@@ -97,16 +67,13 @@ INSERT INTO `user_roles` (`id`, `userId`, `roleId`) VALUES
 --
 
 DROP TABLE IF EXISTS `user_work_positions`;
-CREATE TABLE IF NOT EXISTS `user_work_positions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_work_positions` (
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `work_positionId` int(11) NOT NULL,
   `productName` varchar(40) NOT NULL,
-  `dateCreated` date NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_work_positions_userId_FK` (`userId`),
-  KEY `user_work_positions_work_positionId_FK` (`work_positionId`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `dateCreated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -115,19 +82,71 @@ CREATE TABLE IF NOT EXISTS `user_work_positions` (
 --
 
 DROP TABLE IF EXISTS `work_positions`;
-CREATE TABLE IF NOT EXISTS `work_positions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `work_positions` (
+  `id` int(11) NOT NULL,
   `name` varchar(40) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 --
--- Constraints for table `user_roles`
+-- Indexes for table `roles`
 --
-ALTER TABLE `user_roles`
-  ADD CONSTRAINT `role_roleId_FK` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`),
-  ADD CONSTRAINT `user_userId_FK` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usernameIndex` (`username`);
+
+--
+-- Indexes for table `user_work_positions`
+--
+ALTER TABLE `user_work_positions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_work_positions_userId_FK` (`userId`),
+  ADD KEY `user_work_positions_work_positionId_FK` (`work_positionId`);
+
+--
+-- Indexes for table `work_positions`
+--
+ALTER TABLE `work_positions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user_work_positions`
+--
+ALTER TABLE `user_work_positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `work_positions`
+--
+ALTER TABLE `work_positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
 
 --
 -- Constraints for table `user_work_positions`
